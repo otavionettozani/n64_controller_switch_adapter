7a0006a4aef41e35c5e9a43d7ca7ca37324e09b2
Otávio Netto Zani <otavionettozani@gmail.com>
2022-09-25 17:42:24 -0300

    adding tests to PRU

---
+++ pru_tests/README.md
@@ -0,0 +1,12 @@
+PRU Location
+/sys/class/remoteproc/remoteprocN
+
+start PRU
+echo start > /sys/class/remoteproc/remoteprocN/state
+
+stop PRU
+echo stop > /sys/class/remoteproc/remoteprocN/state
+
+load compiled program:
+cp dist_lib/*.out /lib/firmware/program_name
+echo program_name > /sys/class/remoteproc/remoteprocN/firmware
---
+++ pru_tests/hw.c
@@ -0,0 +1,26 @@
+#include <stdin.h>
+#include <rsc_types.h>
+#include "./r30_pins.c"
+
+#define CYCLES_PER_SECOND 200000000
+
+volatile register uint32_t __R30;
+volatile register uint32_t __R31;
+
+void main(void) {
+  while (1) {
+    __R30 |= P9_31;
+    __delay_cycles(CYCLES_PER_SECOND/2);
+    __R30 &= ~P9_31;
+    __delay_cycles(CYCLES_PER_SECOND/2);
+  }
+}
+
+
+/* required by PRU */
+#pragma DATA_SECTION(pru_remoteproc_ResourceTable, ".resource_table")
+#pragma RETAIN(pru_remoteproc_ResourceTable)
+struct my_resource_table {
+    struct resource_table base;
+    uint32_t offset[1];
+} pru_remoteproc_ResourceTable = { 1, 0, 0, 0, 0 };
